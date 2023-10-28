6a97b3ff90c7a4bda1e797d5285cb10912c5d445
th3bossc <diljith2003@gmail.com>
2023-10-28 23:10:20 +0530

    adjusted admin portal

--- backend/tfms/urls.py
+++ backend/tfms/urls.py
@@ -18,6 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+admin.site.site_header = 'Traffic Fine Management System'
+admin.site.site_title = 'Admin Portal'
+admin.site.index_title = 'Welcome to TFMS Portal'
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
